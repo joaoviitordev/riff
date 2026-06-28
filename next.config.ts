0c8cf9bae551262e877e3a1742b0d7b26cef3707
joaoviitordev <joaovitorgomesdefaria@gmail.com>
2026-06-28 12:54:51 -0300

    feat(design): add svg favicon configuration and favicon.ico rewrite

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/favicon.ico",
+        destination: "/riff-favicon.svg",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
