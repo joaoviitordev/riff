c89a82cc96e7cf667ec16ef66d0c78c02a86b190
joaoviitordev <joaovitorgomesdefaria@gmail.com>
2026-06-28 14:14:01 -0300

    feat: add favicon and logo component to the application

--- components/logo.tsx
+++ components/logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
       data-purpose="logo-header"
     >
       <h1 className="text-white text-6xl font-bold tracking-tighter flex items-end">
-        riff<span className="text-riff-orange text-5xl leading-none">.</span>
+        Riff<span className="text-riff-orange text-5xl leading-none">.</span>
       </h1>
     </header>
   );
